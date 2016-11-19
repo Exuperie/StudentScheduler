91b5ac92b804ab0f601b07cc11b1664e31074b1b
Yichi Zhang <yichi.zhang@mail.mcgill.ca>
2016-11-19 12:45:45 -0500

    json file

--- Program.py
+++ Program.py
@@ -1,12 +1,30 @@
 from Student import Student
 import json
 
+classList = []
 studentList = []
-Candelario = Student(1, "Candelario", "10:00-12:00", None)
+Candelario = Student(1)
 studentList.append(Candelario)
 
-with open('classes.json') as data_file:
+with open('studentsByAvailability.json') as data_file:
 	data = json.load(data_file)
-	data1 = data["classes"]["101"]["name"]
 
-print data1
+	for key, value in data.iteritems():
+		student = Student(key)
+		for items in value:
+
+			if type(items) is unicode:
+				print items
+
+			if type(items) is dict:
+				for key, value in items.iteritems():
+					print key
+					print value["day"]
+					print value["start"]
+					print value["end"]
+
+
+
+
+
+
