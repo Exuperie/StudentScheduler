153dc7a4f69da5e95ddaa5dfed9503da0375570a
Exuperie <weijia.shi@mail.mcgill.ca>
2016-11-19 15:05:50 -0500

    compare

--- compare.py
+++ compare.py
@@ -1,11 +1,17 @@
 #this takes two time points expressed as floats 
 #check for conflict 
 
-ctime = [[108.5, 110.5], [115, 119]]
-stime = [108.5, 112]
+ctime1 = [108.5, 110.5]
+stime1 = [[108.5, 112], [115, 117]]
 
-def compare():
+def compare(ctime, stime):
 	i = 0
-	while i < len(ctime):
-		ctime
-	print foo()
+	#loop through student avail1
+	while i < len(stime):
+		if stime[i][0]<=ctime[0] and stime[i][1]>=ctime[1]: 
+			return True
+		else: return False
+		i+=1
+
+		
+print compare(ctime1, stime1)
